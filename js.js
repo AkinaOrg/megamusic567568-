31e971485073b7f6d4c7b7891bb1456f8abb26a0
LOH567568 <90637155+LOH567568@users.noreply.github.com>
2022-03-10 09:33:32 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+let sayHi = () => alert("привет");
+
+sayHi();
+
+let age = prompt("Сколько Вам лет?", );
+
+let welcome = (age < 18) ?
+  () => alert('Привет') :
+
+
+welcome();
---
+++ yaer.js
@@ -0,0 +1,31 @@
+my_name = 'Svetlana'
+console.log(`Hello, ${my_name}`)
+
+const year = 2021
+if (1000 < 2021) {
+   console.log("1000<2021")
+} else {
+    console.log("1000>2021")
+}
+age = prompt("имя: ")
+alert(`привет: ${age}`)
+
+if (age>=1 && age<=3) {
+    console.log('Раннее детство')
+}
+if (age>=4 && age<=7){
+    console.log('Дошкольный возраст')
+}
+if (age>=8 && age<=13){
+    console.log('Школьный возраст')
+}
+if (age>=14 && age<=17){
+    console.log('Пубертатный возраст ')
+}
+if (age >= 60) {
+    console.log('Старость')
+}
+else {
+    console.log('другое значение')
+}
+
